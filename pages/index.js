95e6f53b42fba23cff48791a4d9e7dafd2beda52
oahehc <oahehc@gmail.com>
2020-06-07 10:11:05 +0800

    Feature: change to public variable

--- pages/index.js
+++ pages/index.js
@@ -2,7 +2,7 @@ import React from "react";
 
 const envVariables = [
   { name: "URL", value: process.env.VERCEL_URL },
-  { name: "ENVIRONMENT", value: process.env.ENVIRONMENT },
+  { name: "ENVIRONMENT", value: process.env.NEXT_PUBLIC_ENVIRONMENT },
   { name: "Branch", value: process.env.VERCEL_GITHUB_COMMIT_REF },
   { name: "Commit", value: process.env.VERCEL_GITHUB_COMMIT_SHA },
 ];
