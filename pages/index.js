224914b21002d6f91cdf196105430e078f33d5a1
oahehc <oahehc@gmail.com>
2020-06-07 10:29:13 +0800

    Feature: apply env for server side

--- pages/index.js
+++ pages/index.js
@@ -1,22 +1,33 @@
 import React from "react";
 
 const envVariables = [
-  { name: "URL", value: process.env.VERCEL_URL },
-  { name: "ENVIRONMENT", value: process.env.NEXT_PUBLIC_ENVIRONMENT },
-  { name: "Branch", value: process.env.VERCEL_GITHUB_COMMIT_REF },
-  { name: "Commit", value: process.env.VERCEL_GITHUB_COMMIT_SHA },
+  { name: "URL", clientValue: process.env.VERCEL_URL, serverKey: "url" },
+  {
+    name: "Branch",
+    clientValue: process.env.VERCEL_GITHUB_COMMIT_REF,
+    serverKey: "branch",
+  },
+  {
+    name: "Commit",
+    clientValue: process.env.VERCEL_GITHUB_COMMIT_SHA,
+    serverKey: "commit",
+  },
+  {
+    name: "NEXT_PUBLIC_ENVIRONMENT",
+    clientValue: process.env.NEXT_PUBLIC_ENVIRONMENT,
+    serverKey: "env",
+  },
 ];
 
-const Home = () => (
+const Home = (props) => (
   <div>
     <div className="hero">
-      {envVariables.map(({ name, value }, index) => (
-        <div key={index}>
-          {name}: {value}
+      {envVariables.map(({ name, clientValue, serverKey }) => (
+        <div key={serverKey}>
+          {name}: {clientValue} | {props[serverKey]}
         </div>
       ))}
     </div>
-
     <style jsx>{`
       .hero {
         width: 100%;
@@ -25,5 +36,16 @@ const Home = () => (
     `}</style>
   </div>
 );
+
+export async function getStaticProps() {
+  return {
+    props: {
+      url: process.env.VERCEL_URL,
+      env: process.env.NEXT_PUBLIC_ENVIRONMENT,
+      branch: process.env.VERCEL_GITHUB_COMMIT_REF,
+      commit: process.env.VERCEL_GITHUB_COMMIT_SHA,
+    },
+  };
+}
 
 export default Home;
